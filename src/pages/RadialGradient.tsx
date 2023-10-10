e23459bb3d34f34c6417aef224f1ab95c2086942
ajay-mandaviya <ajay.mandaviya03@gmail.com>
2023-10-11 00:09:27 +0530

    counter increment

--- src/pages/RadialGradient.tsx
+++ src/pages/RadialGradient.tsx
@@ -1,10 +1,13 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import {
+  MotionValue,
   motion,
   stagger,
   useAnimate,
   useMotionTemplate,
   useMotionValue,
+  useSpring,
+  useTransform,
 } from "framer-motion";
 const RadialGradient = () => {
   let [items, setItems] = useState([
@@ -64,6 +67,7 @@ const RadialGradient = () => {
       );
     }
   }
+  let [count, setCount] = useState(0);
 
   return (
     <>
@@ -101,13 +105,9 @@ const RadialGradient = () => {
       <div className="flex min-h-full flex-col items-center justify-center">
         <div className="flex w-full max-w-sm flex-col rounded bg-gray-100 px-3 py-4 shadow-xl">
           <p className="ml-2 flex items-center text-lg font-semibold text-gray-700">
-            {/* <ListBulletIcon className="mr-3 h-5 w-5" /> */}
             Checklist
           </p>
 
-          {/*
-           🟢 Attach the ref from `useAnimate()` to scope the animate() function to this subtree.
-        */}
           <div className="mt-4" ref={ref}>
             {items.map((item) => (
               <label
@@ -128,8 +128,85 @@ const RadialGradient = () => {
           </div>
         </div>
       </div>
+      <div className="flex h-full items-center justify-center">
+        <div className="flex w-1/2 justify-center">
+          <div className="flex-col text-center">
+            <p>Count: {count}</p>
+            <div className="mt-4">
+              <input
+                type="number"
+                value={count}
+                min={0}
+                onChange={(e) => setCount(+e.target.value)}
+                className="w-20 p-1"
+              />
+            </div>
+          </div>
+        </div>
+        <div className="flex w-1/2 items-end justify-center">
+          <Counter value={count} />
+        </div>
+      </div>
     </>
   );
 };
 
+function Counter({ value }: { value: number }) {
+  const animatedValue = useSpring(value);
+  useEffect(() => {
+    animatedValue?.set(value);
+  }, [animatedValue, value]);
+  return (
+    <div className="flex h-6 ring-2 ring-red-500 overflow-hidden">
+      <div className="relative w-6">
+        {[...Array(10).keys()].map((i) => (
+          <Number mv={animatedValue} place={100} number={i} key={i} />
+        ))}
+      </div>
+      <div className="relative w-6">
+        {[...Array(10).keys()].map((i) => (
+          <Number mv={animatedValue} place={10} number={i} key={i} />
+        ))}
+      </div>
+      <div className="relative w-6">
+        {[...Array(10).keys()].map((i) => (
+          <Number mv={animatedValue} place={1} number={i} key={i} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+function Number({
+  place,
+  mv,
+  number,
+}: {
+  place: number;
+  mv: MotionValue;
+  number: number;
+}) {
+  let y = useTransform(mv, (latest: any) => {
+    let height = 24;
+    let placeValue = (latest / place) % 10;
+    let offset = (10 + number - placeValue) % 10;
+
+    let memo = offset * height;
+    if (offset > 5) {
+      memo -= 10 * height;
+    }
+    return memo;
+  });
+
+  return (
+    <motion.span
+      style={{
+        y,
+      }}
+      className="absolute inset-0 flex justify-center"
+    >
+      {number}
+    </motion.span>
+  );
+}
 export default RadialGradient;
